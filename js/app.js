dbb58109acc18942dfab67216dbc079f3c4dbd80
CyberKYD <lucas25rock@gmail.com>
2026-01-19 14:42:26 -0300

    diseño-responsivo-fixes

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const botonBurger = document.getElementById('burger');
+const misBotones = document.getElementById('botones-mas')
+const navBar = document.getElementById('nav-menu')
+
+botonBurger.addEventListener("click" , function(event){
+    event.preventDefault();
+    navBar.classList.add('max-[600px]:block')
+    misBotones.classList.add('max-[600px]:flex')
+});
